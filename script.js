64495b0845bfd4024cdb6472b3b758a2c013d25f
ヌリ <neuly1231@gmail.com>
2025-12-24 22:17:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -261,7 +261,7 @@ function setupImageModal() {
 
 // === 저장 관련 설정 ===
 const captureOptions = {
-    scale: 2,
+    scale: 3,
     useCORS: true,
     backgroundColor: "#cad1dc",
     logging: false,
@@ -384,3 +384,4 @@ function saveAsImage(receiverName) {
         link.click();
     });
 }
+
